2121961a1bd611e5b7cdd0ec046ffc6dfbede4a2
AgustinChavero <agustindanielchavero@gmail.com>
2023-09-18 13:22:29 -0300

    fix

---
+++ api/index.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+const { PORT } = process.env;
+
+conn.sync({ alter: true }).then(() => {
+  server.listen(PORT, () => {
+    console.log("%s listening at 3001");
+  });
+});
